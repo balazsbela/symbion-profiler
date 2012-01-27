2ee3aec2c7f4fcc6be7abb78d7f06a16ff7a3809
balazsbela <balazsbela@gmail.com>
2012-01-27 16:11:22 +0200

    Two node an arrow.

--- symbion-visualizer/src/main/java/org/balazsbela/symbion/visualizer/Visualizer.java
+++ symbion-visualizer/src/main/java/org/balazsbela/symbion/visualizer/Visualizer.java
@@ -1,15 +1,22 @@
 package org.balazsbela.symbion.visualizer;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.font.Rectangle;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.post.filters.BloomFilter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.BillboardControl;
 import com.jme3.texture.Texture;
@@ -18,29 +25,32 @@ import com.jme3.ui.Picture;
 import com.jme3.util.SkyFactory;
 
 public class Visualizer extends SimpleApplication {
+	private Material nodeMat;
 
 	public static void main(String[] args) {
 		Visualizer app = new Visualizer();
 		app.start();
 	}
 
+	public void init() {
+		nodeMat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+		nodeMat.setFloat("Shininess", 32f);
+		nodeMat.setBoolean("UseMaterialColors", true);
+		nodeMat.setColor("Ambient", ColorRGBA.Black);
+		nodeMat.setColor("Diffuse", new ColorRGBA(232f / 256.0f, 227f / 256f, 130f / 256f, 0.7f));
+		nodeMat.setColor("Specular", ColorRGBA.White);
+	}
+
 	@Override
 	public void simpleInitApp() {
 		this.flyCam.setMoveSpeed(20);
-		this.cam.lookAt(new Vector3f(10.0f, 0.0f, 0.0f),new Vector3f(0.0f,1.0f,0.0f));
-		
+		this.cam.lookAt(new Vector3f(0.0f, 0.0f, 0.0f), new Vector3f(0.0f, 1.0f, 0.0f));
+		this.cam.setLocation(new Vector3f(11.0f, 0, 10.0f));
+		init();
 		loadNodes();
 		setupBackground();
 		initLights();
-
 	}
-
-	float angle = FastMath.PI / 8;
-
-	int dirRight = -1;
-	int dirLeft = 1;
-
-	Geometry torosGeom;
 
 	@Override
 	public void simpleUpdate(float tpf) {
@@ -55,45 +65,87 @@ public class Visualizer extends SimpleApplication {
 	}
 
 	private void setupBackground() {
-		 Texture t = assetManager.loadTexture("assets/Textures/bgtexture.png");
-		 Picture p = new Picture("sky");
-		 
-		 p.setTexture(assetManager,(Texture2D)t, false);
-		 p.setWidth(settings.getWidth());
-		 p.setHeight(settings.getHeight());
-		 p.setPosition(0, 0);
-		 p.updateGeometricState();
+
+		Texture t = assetManager.loadTexture("assets/Textures/bgtexture2.png");
+		Picture p = new Picture("sky");
+
+		p.setTexture(assetManager, (Texture2D) t, false);
+		p.setWidth(settings.getWidth());
+		p.setHeight(settings.getHeight());
+		p.setPosition(0, 0);
+		p.updateGeometricState();
+
+		ViewPort pv = renderManager.createPreView("background", cam);
+		pv.setClearFlags(true, true, true);
+		pv.attachScene(p);
+
+		viewPort.setClearFlags(false, true, true);
+
+	}
+
+	private Node createLabeledNode(String labelText) {
+		Node globeNode = new Node(labelText);
+		Spatial nodeModel = assetManager.loadModel("assets/Models/node.j3o");
+		nodeModel.setMaterial(nodeMat);
+
+		BitmapFont hyperion = assetManager.loadFont("assets/Fonts/Hyperion.fnt");
+		BitmapText label = new BitmapText(hyperion);
+		label.setText(labelText);
+		label.setColor(new ColorRGBA(232f / 256.0f, 227f / 256f, 130f / 256f, 0.7f));
+		label.setBox(new Rectangle(-1.0f, 0, 10f, 2f));
+		label.setLocalTranslation(-0.4f, -1.3f, 0.0f);
+		label.setSize(0.2f);
+
+		globeNode.attachChild(nodeModel);
+		globeNode.attachChild(label);
+
+		globeNode.move(new Vector3f(10.0f, 0.0f, 0.0f));
+
+		BillboardControl bc = new BillboardControl();
+		bc.setAlignment(BillboardControl.Alignment.Camera);
+		bc.setEnabled(true);
+		nodeModel.addControl(bc);
+
+		BillboardControl bc1 = new BillboardControl();
+		bc1.setAlignment(BillboardControl.Alignment.Camera);
+		bc1.setEnabled(true);
+		label.addControl(bc1);
+
+		return globeNode;
+	}
+
+	private Node createArrowNode(String arrowNodeName) {
+		Node arrow = new Node(arrowNodeName);
+
+		Spatial arrowBody = assetManager.loadModel("assets/Models/arrowBody.j3o");
+		arrowBody.setMaterial(nodeMat);
+		arrowBody.move(new Vector3f(13.0f, -0.2f, 0f));
+
+		Spatial arrowHead = assetManager.loadModel("assets/Models/arrowHead.j3o");
+		arrowHead.setMaterial(nodeMat);
+		arrowHead.move(new Vector3f(15.0f, 0.05f, 0.335f));
+
+		arrow.attachChild(arrowBody);
+		arrow.attachChild(arrowHead);
 		
-		 ViewPort pv = renderManager.createPreView("background", cam);
-		 pv.setClearFlags(true, true, true);
-		 pv.attachScene(p);
-		
-		 viewPort.setClearFlags(false, true, true);
+		return arrow;
 	}
 
 	private void loadNodes() {
 
-		Material yellow = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-		yellow.setFloat("Shininess", 32f);
-		yellow.setBoolean("UseMaterialColors", true);
-		yellow.setColor("Ambient", ColorRGBA.Black);
-		yellow.setColor("Diffuse",new ColorRGBA(224f/256.0f, 27f/256f, 27f/256f, 0.7f));
-		yellow.setColor("Specular", ColorRGBA.White);
+		Node parent = new Node("parentNode");
+		Node globeNode = createLabeledNode("exampleFunction()");
+		Node arrow = createArrowNode("arrow1");
+		
+		Node globeNode2 = createLabeledNode("exampleCalledFunction()");
+		globeNode2.move(new Vector3f(6.6f,0f,0f));
+	
+		parent.attachChild(globeNode);
+		parent.attachChild(arrow);
+		parent.attachChild(globeNode2);
 
-		
-		Spatial nodeModel = assetManager.loadModel("assets/Models/node.j3o");
-		nodeModel.setMaterial(yellow);
-		nodeModel.move(new Vector3f(10.0f, 0.0f, 0.0f));		
-		rootNode.attachChild(nodeModel);
-		
-		
-		
-		BillboardControl bc = new BillboardControl();
-		//bc.setSpatial(nodeModel);
-		bc.setAlignment(BillboardControl.Alignment.Camera);
-		bc.setEnabled(true);
+		rootNode.attachChild(parent);
 
-		nodeModel.addControl(bc);
 	}
 
 	private void initLights() {
