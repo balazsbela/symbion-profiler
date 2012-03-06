01ad5acbed7b1f4e80b30bd6ba63c4359cc4bc70
balazsbela <balazsbela@gmail.com>
2012-03-07 00:10:55 +0200

    Added stacktrace retrieval

--- symbion-profiler/src/main/java/org/balazsbela/symbion/profiler/BytecodeTransformer.java
+++ symbion-profiler/src/main/java/org/balazsbela/symbion/profiler/BytecodeTransformer.java
@@ -1,12 +1,15 @@
 package org.balazsbela.symbion.profiler;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.balazsbela.symbion.models.*;
 import org.objectweb.asm.ClassAdapter;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
@@ -17,6 +20,7 @@ import static org.balazsbela.symbion.profiler.Log.print;
 public class BytecodeTransformer {
 
 	public static boolean enabled;
+	static EnterExitClassAdapter cv;
 
 	public static byte[] transform(String className, ClassLoader loader, byte[] classBytes, Config config)
 			throws IOException {
@@ -55,6 +59,7 @@ public class BytecodeTransformer {
 		ClassReader cr = new ClassReader(classBytes);
 		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
 		EnterExitClassAdapter ca = new EnterExitClassAdapter(cw, config);
+		BytecodeTransformer.cv = ca;
 		cr.accept(ca, org.objectweb.asm.ClassReader.EXPAND_FRAMES);
 		if (ca.changedMethods > 0) {
 			return cw.toByteArray();
@@ -71,6 +76,10 @@ public class BytecodeTransformer {
 		Type classType;
 		int changedMethods;
 		Config config;
+		public String source;
+		public String methodDesc;
+		public String methodName;
+		public String className;
 
 		public EnterExitClassAdapter(ClassVisitor cv, Config config) {
 			super(cv);
@@ -78,25 +87,34 @@ public class BytecodeTransformer {
 		}
 
 		@Override
+		public void visitSource(String source, String debug) {
+			this.source = source;
+		}
+
+		@Override
 		public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+			className = name;
 			super.visit(version, access, name, signature, superName, interfaces);
 			classType = Type.getType("L" + name + ";");
 		}
 
 		@Override
 		public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+			methodName = name;
+			methodDesc = desc;
+
 			String[] names = makeMethodName(classType, name, desc);
 			String globalName = names[0];
 			String localName = names[1];
 			if (canProfileMethod(classType, access, name, desc, signature, exceptions, config, globalName, localName)) {
-				print(0,"Profiling method:"+globalName);
+				print(0, "Profiling method:" + globalName);
 				if (changedMethods == 0) {
 					Log.print(1, "Instrumenting class " + classType.getClassName());
 				}
 				int gmid = ThreadProfiler.newMethod(globalName);
 				Log.print(3, "    method " + localName);
 				MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
-				EnterExitAdviceAdapter ma = new EnterExitAdviceAdapter(mv, access, name, desc, gmid,globalName);
+				EnterExitAdviceAdapter ma = new EnterExitAdviceAdapter(mv, access, name, desc, gmid, globalName);
 				changedMethods++;
 				return ma;
 			}
@@ -107,8 +125,11 @@ public class BytecodeTransformer {
 	private static class EnterExitAdviceAdapter extends AdviceAdapter {
 		private int gmid;
 		private String methodName;
+		private int line;
+		List<Callee> callees = new ArrayList<Callee>();
 
-		public EnterExitAdviceAdapter(MethodVisitor mv, int access, String name, String desc, int gmid,String globalName) {
+		public EnterExitAdviceAdapter(MethodVisitor mv, int access, String name, String desc, int gmid,
+				String globalName) {
 			super(mv, access, name, desc);
 			this.gmid = gmid;
 			this.methodName = globalName;
@@ -118,15 +139,41 @@ public class BytecodeTransformer {
 		protected void onMethodEnter() {
 			mv.visitLdcInsn(new String(methodName));
 			mv.visitVarInsn(Opcodes.ALOAD, 0);
-			mv.visitMethodInsn(INVOKESTATIC, "org/balazsbela/symbion/profiler/ThreadProfiler", "enterMethod", "(Ljava/lang/String;Ljava/lang/Object;)V");
+			mv.visitMethodInsn(INVOKESTATIC, "org/balazsbela/symbion/profiler/ThreadProfiler", "enterMethod",
+					"(Ljava/lang/String;Ljava/lang/Object;)V");
 		}
 
 		@Override
 		protected void onMethodExit(int opcode) {
 			mv.visitLdcInsn(new String(methodName));
 			mv.visitVarInsn(Opcodes.ALOAD, 0);
-			mv.visitMethodInsn(INVOKESTATIC, "org/balazsbela/symbion/profiler/ThreadProfiler", "exitMethod", "(Ljava/lang/String;Ljava/lang/Object;)V");
+			mv.visitMethodInsn(INVOKESTATIC, "org/balazsbela/symbion/profiler/ThreadProfiler", "exitMethod",
+					"(Ljava/lang/String;Ljava/lang/Object;)V");
 		}
+
+//		@Override
+//		public void visitMethodInsn(int opcode, String owner, String name, String desc) {
+//			if (owner.startsWith("org.balazsbela.FirmManagement")) {
+//				System.out.println("Method call:"+ methodName+"->" + owner + "." + desc);
+//			}
+//			System.out.println();
+//			super.visitMethodInsn(opcode, owner, name, desc);
+//		}
+
+		@Override
+		public void visitLineNumber(int line, Label start) {
+			this.line = line;
+		}
+
+//		@Override
+//		public void visitEnd() {
+//			// System.out.println("Adding to the list of callees of "+
+//			// methodName+" "+cv.className+"."+cv.methodName+ " "+
+//			// cv.methodDesc+" "+cv.source+" "+ line);
+//			// System.out.println();
+//			// callees.add(new Callee(cv.className, cv.methodName,
+//			// cv.methodDesc, cv.source, line));
+//		}
 	}
 
 	private static String[] makeMethodName(Type classType, String methodName, String methodDescriptor) {
@@ -156,7 +203,7 @@ public class BytecodeTransformer {
 		}
 
 		if (globalName.startsWith("org.balazsbela.FirmManagement")) {
-			//Log.print(0, "Checking if can profile:" + globalName);
+			// Log.print(0, "Checking if can profile:" + globalName);
 		}
 
 		List<Rule> rules = config.getRules();
@@ -166,11 +213,12 @@ public class BytecodeTransformer {
 		Rule selectedRule = null;
 		for (Rule rule : rules) {
 			if (globalName.startsWith("org.balazsbela.FirmManagement")) {
-				//Log.print(0, "Checking pattern:" + rule.getPattern() + " for " + globalName);
+				// Log.print(0, "Checking pattern:" + rule.getPattern() +
+				// " for " + globalName);
 			}
 
 			if (rule.matches(globalName)) {
-				//Log.print(0, "Matched rule!");
+				// Log.print(0, "Matched rule!");
 				selectedRule = rule;
 				break;
 			}
--- symbion-profiler/src/main/java/org/balazsbela/symbion/profiler/ThreadProfiler.java
+++ symbion-profiler/src/main/java/org/balazsbela/symbion/profiler/ThreadProfiler.java
@@ -2,84 +2,106 @@ package org.balazsbela.symbion.profiler;
 
 import java.lang.reflect.Field;
 
-
 public class ThreadProfiler {
-	
-    /**
-     * Current number of instrumented methods in current {@link #sessionId}. Whenever a
-     * new session is created this value is set to 0.
-     */
-    private static int methodCount = 0;
-    
-	 /**
-     * Global monitor used to implement mutual-exclusion. In the future this single
-     * monitor may be broken up into many different monitors to reduce contention.
-     */
-    static final Object globalLock = new GlobalLock();
 
-    private static class GlobalLock {
-    }
+	/**
+	 * Current number of instrumented methods in current {@link #sessionId}.
+	 * Whenever a new session is created this value is set to 0.
+	 */
+	private static int methodCount = 0;
 
-    /**
-     * Creates a new method object assigning a new GMId (Global Method Id)
-     * <p>
-     * A global method ID has two fundamental parts: bits 0-15 represent the method ID,
-     * bits 16-23 represent the. This means that global IDs are unique
-     * for any method and for any session in the same JVM execution.
-     * 
-     * @param methodName name of the method to trace
-     * @return global method id
-     */
-    public static int newMethod(String methodName) {
-        synchronized (globalLock) {
-            int globalMethodId = (0 << 16) | methodCount;          
-            return globalMethodId;
-        }
-    }
-    
-    /**
-     * Records a method's entry. This method is called by instrumented code.
-     * 
-     * @param globalMethodId
-     * @throws IllegalAccessException 
-     * @throws IllegalArgumentException 
-     * @see #newMethod(String)
-     */
-    public static void enterMethod(String globalMethod,Object o) {    	
-        final Thread ct = Thread.currentThread();
-   
-        synchronized (globalLock) {
-           System.out.println("Entered:"+ globalMethod);
-           
-           System.out.println("Current class:"+o.getClass().getName());
-           System.out.println("Class has the following fields with the following values:");
-           for(Field field : o.getClass().getDeclaredFields()) {
-        	   try {
-        		field.setAccessible(true);
-				System.out.println(field.getName()+" "+field.get(o));
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	/**
+	 * Global monitor used to implement mutual-exclusion. In the future this
+	 * single monitor may be broken up into many different monitors to reduce
+	 * contention.
+	 */
+	static final Object globalLock = new GlobalLock();
+
+	private static class GlobalLock {
+	}
+
+	/**
+	 * Creates a new method object assigning a new GMId (Global Method Id)
+	 * <p>
+	 * A global method ID has two fundamental parts: bits 0-15 represent the
+	 * method ID, bits 16-23 represent the. This means that global IDs are
+	 * unique for any method and for any session in the same JVM execution.
+	 * 
+	 * @param methodName
+	 *            name of the method to trace
+	 * @return global method id
+	 */
+	public static int newMethod(String methodName) {
+		synchronized (globalLock) {
+			int globalMethodId = (0 << 16) | methodCount;
+			return globalMethodId;
+		}
+	}
+
+	/**
+	 * Records a method's entry. This method is called by instrumented code.
+	 * 
+	 * @param globalMethodId
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @see #newMethod(String)
+	 */
+	public static void enterMethod(String globalMethod, Object o) {
+		final Thread ct = Thread.currentThread();
+
+		synchronized (globalLock) {
+			
+			String methodName = globalMethod.substring(0, globalMethod.indexOf("("));
+			
+			StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+			int index = 0;
+			String currentStMethod;
+			for(int i=0;i<stackTraceElements.length;i++) {
+				currentStMethod = stackTraceElements[i].getClassName()+"."+stackTraceElements[i].getMethodName();
+//				System.out.println(currentStMethod);
+				if(currentStMethod.contains(methodName)) {
+					index=i;
+					break;
+				}
 			}
-           }
-           ExecutionTimeline.calledMethods.add(globalMethod);
-        }
-    }
-    /**
-     * Records a method's exit. This method is called by instrumented code.
-     * 
-     * @param globalMethodId
-     * @see #newMethod(String)
-     */
-    public static void exitMethod(String globalMethod,Object o) {
-        final Thread ct = Thread.currentThread();               
-        synchronized (globalLock) {
-            System.out.println("Exited:"+globalMethod + " from object of class:"+o.getClass().getName());
+			
+			//One deeper in the stack trace
+			StackTraceElement caller = stackTraceElements[index+1];
+			System.out.println("--------------------------------------");
+			System.out.println("Entered:"+caller.getMethodName()+"->"+ globalMethod);
+			
+			System.out.println("Current class:" + o.getClass().getName());
+			System.out.println("Class has the following fields with the following values:");
+			System.out.println("--------------------------------------");
+			for (Field field : o.getClass().getDeclaredFields()) {
+				try {
+					field.setAccessible(true);
+					System.out.println(field.getName() + " " + field.get(o));
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			ExecutionTimeline.calledMethods.add(globalMethod);
+		}
+	}
 
-        }
-    }
+	/**
+	 * Records a method's exit. This method is called by instrumented code.
+	 * 
+	 * @param globalMethodId
+	 * @see #newMethod(String)
+	 */
+	public static void exitMethod(String globalMethod, Object o) {
+		final Thread ct = Thread.currentThread();
+		synchronized (globalLock) {
+			System.out.println("--------------------------------------");
+			System.out.println("Exited:" + globalMethod + " from object of class:" + o.getClass().getName());
+			System.out.println("--------------------------------------");
+		}
+	}
 
 }
